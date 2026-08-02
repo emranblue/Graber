6c757db0feaee0c01c58ce9cd72f60b6ba6d7824
Emran Hossain <63453378+emranblue@users.noreply.github.com>
2026-08-02 13:35:34 +0600

    Enhanced: Add dynamic search header with debouncing and fuzzy matching

--- ui/HeadingSelectDialog.h
+++ ui/HeadingSelectDialog.h
@@ -6,6 +6,7 @@
 #include <QListWidget>
 #include <QKeyEvent>
 #include <QCoreApplication>
+#include <QTimer>
 #include "Types.h"
 
 class HeadingSelectDialog : public QDialog {
@@ -23,16 +24,27 @@ private slots:
     void on_search_text_changed(const QString &text);
     void on_item_double_clicked(QListWidgetItem *item);
     void on_select_clicked();
+    void on_debounce_timer();
 
 private:
+    struct SearchResult {
+        int index;
+        int relevance_score;
+        const NoteItem *item;
+    };
+
     void populate_list(const QString &search_text);
     QString highlight_text(const QString &src, const QStringList &keywords);
+    int calculate_relevance(const NoteItem &item, const QStringList &keywords);
+    bool fuzzy_match(const QString &text, const QString &pattern);
 
     const QList<NoteItem> &all_headings_;
     QString selected_slug_;
     QString selected_title_;
     QLineEdit *search_edit_;
     QListWidget *list_widget_;
+    QTimer *debounce_timer_;
+    QString pending_search_text_;
 };
 
 #endif // HEADINGSELECTDIALOG_H
