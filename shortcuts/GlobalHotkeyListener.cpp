ab54d16ffeb04fd42a8c917d29a83ec4285d60d9
emranblue <emran.blue.120@gmail.com>
2026-08-05 15:55:57 +0600

    Macish

--- shortcuts/GlobalHotkeyListener.cpp
+++ shortcuts/GlobalHotkeyListener.cpp
@@ -21,7 +21,11 @@
 GlobalHotkeyListener::NativeEventFilter::NativeEventFilter(GlobalHotkeyListener *parent)
     : parent_(parent) {}
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+bool GlobalHotkeyListener::NativeEventFilter::nativeEventFilter(const QByteArray &eventType, void *message, qintptr *result) {
+#else
 bool GlobalHotkeyListener::NativeEventFilter::nativeEventFilter(const QByteArray &eventType, void *message, long *result) {
+#endif
     Q_UNUSED(result);
     if (eventType == "windows_generic_MSG") {
         MSG *msg = static_cast<MSG *>(message);
